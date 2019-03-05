dd8187d1bec261a269bfdc21b4bb59fffc6df208
Unknown <mli9502@outlook.com>
2019-03-04 22:36:49 -0500

    Add implementation for Ping and PingReq.

--- cloud_computing_concepts/mp1/mp1/PingMessage.cpp
+++ cloud_computing_concepts/mp1/mp1/PingMessage.cpp
@@ -4,15 +4,61 @@ string PingMessage::getId() {
     return source.getAddress() + "|" + destination.getAddress() + "|" + to_string(protocol_period);
 }
 
-void PingMessage::decode(const string& msg) {
-    // TODO: fill this in.
+void PingMessage::decode(string msg) {
+    char* msgPtr = &(msg[0]);
+    MP1Node::copyObj(msgPtr, this->msgType);
+    MP1Node::copyObj(msgPtr, this->source);
+    MP1Node::copyObj(msgPtr, this->destination);
+    MP1Node::copyObj(msgPtr, this->protocol_period);
+    MembershipList::decodeTopKMsg(msgPtr, this->piggybackMembershipList);
+    FailList::decodeTopKMsg(msgPtr, this->piggybackFailList);
 }
 
+// pb: piggyback
+// ml: membership_list
+// fl: fail_list
+// MsgType|src_address|dest_address|protocol_period|pb_ml_size|pb_ml|pb_fl_size|pb_fl|
 string PingMessage::encode() {
-    // TODO: fill this in.
-    return "";
+    string piggybackMembershipListMsg = MembershipList::encodeTopKMsg(this->piggybackMembershipList);
+    string piggybackFailListMsg = FailList::encodeTopKMsg(this->piggybackFailList);
+    
+    unsigned msgSize = sizeof(MsgTypes) // MsgType
+                        + sizeof(Address) // src_address
+                        + sizeof(Address) // dest_address
+                        + sizeof(unsigned long) // protocol_period
+                        + sizeof(unsigned) // pb_ml_size
+                        + piggybackMembershipListMsg.size() // pb_ml
+                        + sizeof(unsigned) // pb_fl_size
+                        + piggybackFailList.size(); // pb_fl
+    
+    char* msg = new char[msgSize];
+    auto msgStart = msg;
+    MsgTypes type = MsgTypes::PING;
+    MP1Node::copyMsg(msgStart, type);
+    MP1Node::copyMsg(msgStart, this->source);
+    MP1Node::copyMsg(msgStart, this->destination);
+    MP1Node::copyMsg(msgStart, this->protocol_period);
+    unsigned pbMlSize = this->piggybackMembershipList.size();
+    MP1Node::copyMsg(msgStart, pbMlSize);
+    memcpy(msgStart, piggybackMembershipListMsg.c_str(), piggybackMembershipListMsg.size());
+    msgStart += piggybackMembershipListMsg.size();
+    unsigned pbFlSize = this->piggybackFailList.size();
+    MP1Node::copyMsg(msgStart, pbFlSize);
+    memcpy(msgStart, piggybackFailListMsg.c_str(), piggybackFailListMsg.size());
+    msgStart += piggybackFailListMsg.size();
+
+    string rtn(msg, msgSize);
+#ifdef DEBUGLOG
+    cout << "In PingMessage::encode()..." << endl;
+    cout << "msgSize: " << msgSize << endl;
+    cout << "msg: " << rtn << endl;
+#endif
+    return rtn;
 }
 
 void onReceiveHandler(MP1Node& state) {
     // TODO: fill this in.
+#ifdef DEBUGLOG
+    cout << "In PingMessage::onReceiveHandler..." << endl;
+#endif
 }
--- cloud_computing_concepts/mp1/mp1/PingReqMessage.cpp
+++ cloud_computing_concepts/mp1/mp1/PingReqMessage.cpp
@@ -5,15 +5,64 @@ string PingReqMessage::getId() {
 }
 
 void PingReqMessage::decode(string msg) {
-    // TODO: fill this in.        
+    char* msgPtr = &(msg[0]);
+    MP1Node::copyObj(msgPtr, this->msgType);
+    MP1Node::copyObj(msgPtr, this->source);
+    MP1Node::copyObj(msgPtr, this->route);
+    MP1Node::copyObj(msgPtr, this->destination);
+    MP1Node::copyObj(msgPtr, this->protocol_period);
+    MembershipList::decodeTopKMsg(msgPtr, this->piggybackMembershipList);
+    FailList::decodeTopKMsg(msgPtr, this->piggybackFailList);    
 }
 
+// pb: piggyback
+// ml: membership_list
+// fl: fail_list
+// MsgType|src_address|route|dest_address|protocol_period|pb_ml_size|pb_ml|pb_fl_size|pb_fl|
 string PingReqMessage::encode() {
-    // TODO: fill this in.
-    return "";
+    string piggybackMembershipListMsg = MembershipList::encodeTopKMsg(this->piggybackMembershipList);
+    string piggybackFailListMsg = FailList::encodeTopKMsg(this->piggybackFailList);
+    
+    unsigned msgSize = sizeof(MsgTypes) // MsgType
+                        + sizeof(Address) // src_address
+                        + sizeof(Address) // route
+                        + sizeof(Address) // dest_address
+                        + sizeof(unsigned long) // protocol_period
+                        + sizeof(unsigned) // pb_ml_size
+                        + piggybackMembershipListMsg.size() // pb_ml
+                        + sizeof(unsigned) // pb_fl_size
+                        + piggybackFailList.size(); // pb_fl
+    
+    char* msg = new char[msgSize];
+    auto msgStart = msg;
+    MsgTypes type = MsgTypes::PING;
+    MP1Node::copyMsg(msgStart, type);
+    MP1Node::copyMsg(msgStart, this->source);
+    MP1Node::copyMsg(msgStart, this->route);
+    MP1Node::copyMsg(msgStart, this->destination);
+    MP1Node::copyMsg(msgStart, this->protocol_period);
+    unsigned pbMlSize = this->piggybackMembershipList.size();
+    MP1Node::copyMsg(msgStart, pbMlSize);
+    memcpy(msgStart, piggybackMembershipListMsg.c_str(), piggybackMembershipListMsg.size());
+    msgStart += piggybackMembershipListMsg.size();
+    unsigned pbFlSize = this->piggybackFailList.size();
+    MP1Node::copyMsg(msgStart, pbFlSize);
+    memcpy(msgStart, piggybackFailListMsg.c_str(), piggybackFailListMsg.size());
+    msgStart += piggybackFailListMsg.size();
+
+    string rtn(msg, msgSize);
+#ifdef DEBUGLOG
+    cout << "In PingReqMessage::encode()..." << endl;
+    cout << "msgSize: " << msgSize << endl;
+    cout << "msg: " << rtn << endl;
+#endif
+    return rtn;
 }
 
 void onReceiveHandler(MP1Node& state) {
     // TODO: fill this in.
+#ifdef DEBUGLOG
+    cout << "In PingReqMessage::onReceiveHandler..." << endl;
+#endif    
 }
 
